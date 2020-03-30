1afb3e47ebaf031643d170ff0e98f41e809ce7fd
Reef Horvitz <reefhorvitz@gmail.com>
2020-03-30 18:18:54 +0300

    fixed map

--- src/Components/PropertyDetails/PropertySlideshow/index.js
+++ src/Components/PropertyDetails/PropertySlideshow/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import MapImage from '../../../static/images/map.png';
 import ImageSlide from "../../../Containers/ImageSlide";
+import styles from './styles.module.css';
 
 const PropertySlideshow = ({propertyId}) => {
     return (
         <section>
-            <div className="location-slider hide-slid-sm">
+            <div className={`location-slider hide-slid-sm ${styles.container}`}>
                 <div className="apartment">
                     <div id="container" className="cf">
                         <div id="main" role="main">
@@ -13,7 +14,7 @@ const PropertySlideshow = ({propertyId}) => {
                         </div>
                     </div>
                 </div>
-                <div className="map-sm"><img src={MapImage} className="img-responsive"/></div>
+                <div className={`map-sm ${styles.mapContainer}`}><img src={MapImage} className="img-responsive"/></div>
             </div>
         </section>
 );
